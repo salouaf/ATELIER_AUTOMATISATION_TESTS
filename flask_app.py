f82ef59a7e6f72db1ed80de2291ddb09cca671da
salouaf <s.florand@cfacampusmontsouris.fr>
2026-03-11 16:54:33 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -5,35 +5,11 @@ from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
 
-
 app = Flask(__name__)
 
 @app.get("/")
 def consignes():
      return render_template('consignes.html')
-
-
-@app.get("/plat")
-def plat_aleatoire():
-    # Appel de l'API Foodish
-    url = "https://foodish-api.com/api/"
-    response = request.get(url)
-    
-    # Vérifier que la requête a fonctionné
-    if response.status_code != 200:
-        return "Erreur lors de l'appel à l'API", 500
-    
-    # Récupérer le JSON
-    data = response.json()
-    image_url = data.get("image", "")
-    
-    # Afficher l'image dans le navigateur
-    html = f"""
-    <h1>Plat aléatoire</h1>
-    <img src="{image_url}" alt="Plat" style="max-width:500px;">
-    <br><a href='/plat'>Voir un autre plat</a>
-    """
-    return html
 
 if __name__ == "__main__":
     # utile en local uniquement
