aee03da9fa37c31eabfecb73955928e5e73eca6b
salouaf <s.florand@cfacampusmontsouris.fr>
2026-03-11 16:37:34 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,15 +1,13 @@
 import requests
 
-# Test 1 : Vérifier le statut HTTP
 def test_status_http():
-    url = "https://foodish-api.com/api/"
+    url = "https://foodish-api.com/api/"  # URL complète de Foodish
     response = requests.get(url)
-    assert response.status_code == 200  # la requête doit réussir
+    assert response.status_code == 200
 
-# Test 2 : Vérifier que le JSON contient le champ 'image'
 def test_json_et_champ_image():
     url = "https://foodish-api.com/api/"
     response = requests.get(url)
-    data = response.json()  # transforme la réponse en dictionnaire
-    assert "image" in data        # le champ 'image' existe
-    assert isinstance(data["image"], str)  # c’est une chaîne
+    data = response.json()
+    assert "image" in data
+    assert isinstance(data["image"], str)
